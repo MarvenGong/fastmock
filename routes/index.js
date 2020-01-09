fce7755490a58dc21cbe6b780598b41505374526
01477209 <gongminghua@haier.com>
2020-01-09 11:18:47 +0800

    dev

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('build', { title: 'HOME' });
+  res.render('index/index', { title: 'HOME' });
 });
 module.exports = router;
